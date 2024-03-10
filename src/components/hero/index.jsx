a31501f586580a0e27cc984b09bc37f958e1c104
alvinkenyagah <alvin47k@gmail.com>
2024-03-10 17:42:41 +0300

    Compress images

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -6,6 +6,6 @@ import t5 from "./images/img5.jpg";
 import t6 from "./images/img6.jpg";
 import t7 from "./images/img7.jpg";
 import t8 from "./images/img8.jpg";
-import t9 from "./images/img9.jpg";
 
-export { t1, t2, t3, t4, t5, t6, t7, t8, t9 };
+
+export { t1, t2, t3, t4, t5, t6, t7, t8 };
